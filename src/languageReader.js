80675088ecafbaff214fd68e0c22c1e75c95c267
River-Langwell <knapp.karla@gmail.com>
2026-01-04 18:09:38 -0500

    troubleshooting

--- src/languageReader.js
+++ src/languageReader.js
@@ -8,9 +8,10 @@ export class LanguageLibrary {
     getLibrary() {
         var map = new Map();
         const file = fs.readFileSync(`${import.meta.dirname}/languageValues.csv`, 'utf-8');
-        console.log(file);
-        
+        //console.log(file);
+
         file.split(/r?\n/).forEach(line => {
+            console.log(line);
             map.set(line.split(',')[0], new languageEntry(line.split(',')));
         });
 
