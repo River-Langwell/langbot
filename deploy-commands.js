9955a98c3e46265f24e85f8a829bbb2b63e24e6f
River-Langwell <knapp.karla@gmail.com>
2026-01-03 21:10:15 -0500

    fixing deploy yet again

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 import { REST, Routes } from 'discord.js';
 import config from './config.json' with {type: "json"};
-import { readdirSync } from 'node:fs';
-import { join } from 'node:path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 
 const clientId = config.clientId;
@@ -11,16 +11,16 @@ const discord_baseurl = config.discord_baseurl;
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = join(__dirname, 'commands');
-const commandFolders = readdirSync(foldersPath);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
-    const commandsPath = join(foldersPath, folder);
-    const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
-        const filePath = join(commandsPath, file);
+        const filePath = path.join(commandsPath, file);
         const command = require(filePath);
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
