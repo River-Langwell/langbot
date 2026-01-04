6adc5de2953bebefb2098cfd103a7bb59ed8b414
River-Langwell <knapp.karla@gmail.com>
2026-01-03 21:17:14 -0500

    attempt

--- deploy-commands.js
+++ deploy-commands.js
@@ -21,7 +21,7 @@ for (const folder of commandFolders) {
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+        const command = import(filePath);
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
         } else {
@@ -29,6 +29,7 @@ for (const folder of commandFolders) {
         }
     }
 }
+
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
