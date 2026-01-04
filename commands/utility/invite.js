3b3d30751a5104fcc0e21c34e090c8388693243f
River-Langwell <knapp.karla@gmail.com>
2026-01-03 21:24:20 -0500

    update

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -1,8 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { applicationId } from './config.json';
 
-module.exports = {
-
+export const exports = {
     data: new SlashCommandBuilder().setName('invite').setDescription('Replies with a bot invite'),
     async execute(interaction) {
         await interaction.reply(`https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`);
