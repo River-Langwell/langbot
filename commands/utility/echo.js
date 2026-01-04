ed50f5c47806ef45c6c3d1f660ec7c42ee28870b
River-Langwell <knapp.karla@gmail.com>
2026-01-03 23:01:19 -0500

    forgot to comment this back out

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
     .setName('echo')
     .setDescription('Replies with your input!')
-    .addStringOption((option) => option.setName('input').setDescription('The input to echo back')).setRequired(true)
+    .addStringOption((option) => option.setName('input').setDescription('The input to echo back'))//.setRequired(true)
     .addChannelOption((option) => option.setName('channel').setDescription('The channel to echo into'))
     .addBooleanOption((option) => option.setName('ephemeral').setDescription('Whether or not the echo should be ephemeral')
     );
