4e3b17eb23365e0360dc8d4e9980dcc11d62279d
River-Langwell <knapp.karla@gmail.com>
2026-01-04 18:23:14 -0500

    update

--- src/languageReader.js
+++ src/languageReader.js
@@ -5,7 +5,7 @@ export class LanguageLibrary {
 
         this.map = this.getLibrary();
 
-        this.map.forEach(value, key => {
+        this.map.forEach(key => {
             console.log(key);
         });
     }
