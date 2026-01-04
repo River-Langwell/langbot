2bc7f4fab996fc0cec27c78b45ecd18cf58f33e0
River-Langwell <knapp.karla@gmail.com>
2026-01-04 17:25:11 -0500

    update

--- src/languageReader.js
+++ src/languageReader.js
@@ -7,7 +7,7 @@ export class LanguageLibrary {
 
     getLibrary() {
         var map = new Map();
-        file = open('./languageValues.csv');
+        file = fs.open('./languageValues.csv');
         for (const line of file.readLines()) {
             let l = line.split(',')[0];
 
