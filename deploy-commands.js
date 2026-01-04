c2b58487413b3cfff3526fbba73df4ad69d6a591
River-Langwell <knapp.karla@gmail.com>
2026-01-03 22:37:34 -0500

    debug

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,6 +17,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
     const commandsPath = path.join(foldersPath, folder);
+    console.log(comandsPath);
     const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
