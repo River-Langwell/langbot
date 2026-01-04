9fad19c29adbc3869b386840682016a7e3834009
River-Langwell <knapp.karla@gmail.com>
2026-01-03 21:38:41 -0500

    update

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -1,13 +1,14 @@
 import { SlashCommandBuilder } from 'discord.js';
 
-export const data = new SlashCommandBuilder()
-    .setName('echo')
-    .setDescription('Replies with your input!')
-    .addStringOption((option) => option.setName('input').setDescription('The input to echo back')).setRequired(true)
-    .addChannelOption((option) => option.setName('channel').setDescription('The channel to echo into'))
-    .addBooleanOption((option) => option.setName('ephemeral').setDescription('Whether or not the echo should be ephemeral')
-    );
-    
-export async function execute(interaction) {
-    await interaction.reply(interaction.option[0]);
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input!')
+        .addStringOption((option) => option.setName('input').setDescription('The input to echo back')).setRequired(true)
+        .addChannelOption((option) => option.setName('channel').setDescription('The channel to echo into'))
+        .addBooleanOption((option) => option.setName('ephemeral').setDescription('Whether or not the echo should be ephemeral')
+        ),
+    async execute(interaction) {
+        await interaction.reply(interaction.option[0]);
+    }
 }
