24b7d06580daa50cdd5417c5fc191451f1f44098
River-Langwell <knapp.karla@gmail.com>
2026-01-12 18:00:41 -0500

    purge

--- commands/utility/purge.js
+++ commands/utility/purge.js
@@ -19,7 +19,7 @@ export async function execute(interaction) {
 
         console.log(guild.name);
 
-        const channels = guild.channels.fetch().array();
+        const channels = guild.channels.fetch();
 
         channels.forEach(c => {
             console.log(c.name);
