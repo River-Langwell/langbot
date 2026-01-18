19633e49222395eafbe86f9595c3923730368a38
River-Langwell <knapp.karla@gmail.com>
2026-01-17 19:41:49 -0500

    ha

--- commands/utility/purge.js
+++ commands/utility/purge.js
@@ -3,26 +3,16 @@ import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
     .setName('purge')
-    .setDescription('purge all messages from the selected channel(s).')
-    .addChannelOption((option) => option.setName('channel').setDescription('The channel to purge all messages from; if no channel is selected, purging from default(s).'))
+    .setDescription('purge all messages from the current channel.')
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels | PermissionFlagsBits.ManageMessages);
 
 export async function execute(interaction) {
 
     await interaction.reply('Beginning purge.');
-
     try {
-
+        
         const client = interaction.client;
-
-        //console.log(interaction.guildId);
-
-        const guild = await client.guilds.fetch(interaction.guildId);
-
-        console.log(guild.name);
-
         const channel = await client.channels.fetch(interaction.channelId);
-        console.log(channel.name);
 
         while (true) {
             let fetchedMessages = await channel.messages.fetch({ limit: 100, before: interaction.id });
