2e1585251f3bd5c3c86766531669ec612cda1aaa
River-Langwell <knapp.karla@gmail.com>
2026-01-17 19:47:03 -0500

    adjusting to admin only

--- commands/utility/purge.js
+++ commands/utility/purge.js
@@ -4,13 +4,13 @@ import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
     .setName('purge')
     .setDescription('purge all messages from the current channel.')
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels | PermissionFlagsBits.ManageMessages);
+    .setDefaultMemberPermissions(0);
 
 export async function execute(interaction) {
 
     await interaction.reply('Beginning purge.');
     try {
-        
+
         const client = interaction.client;
         const channel = await client.channels.fetch(interaction.channelId);
 
