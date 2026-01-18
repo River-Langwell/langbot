449f0134a6e701bdc679cef0f7d1ab99f00738f7
River-Langwell <knapp.karla@gmail.com>
2026-01-17 19:22:12 -0500

    update

--- commands/utility/purge.js
+++ commands/utility/purge.js
@@ -24,13 +24,10 @@ export async function execute(interaction) {
         console.log(channel.name);
 
         while (true) {
-
-            
             const fetchedMessages = await channel.messages.fetch({ limit: 100, before: interaction.id })
                 .then(data => {
-                    if (fetchedMessages.size === 0) { }
                     let messageArr = [...data];
-                    console.log(data[0][1]);
+                    console.log(messageArr[0][1]);
                 });
 
         }
